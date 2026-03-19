7d1a81af0a44359fa09110b8f75832a52798b074
cuttlefish <cuttlefish256@gmail.com>
2026-03-20 03:21:33 +0800

    Added model relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
-  belongs_to :community
-  belongs_to :seller
+  belongs_to :community, optional: true
+  belongs_to :seller, class_name: User
+
+  enum :condition, { poor: 0, fair: 1, good: 2, like_new: 3, brand_new: 4 }
+  enum :status, { available: 0, reserved: 1, sold: 2 }
 end

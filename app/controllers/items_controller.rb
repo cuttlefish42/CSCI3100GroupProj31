438b358084d369548ae0d245e8afb8359c03c246
cuttlefish <cuttlefish256@gmail.com>
2026-03-20 23:28:30 +0800

    added items controllers for CRUD operations.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[ index show ]
+
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.seller = Current.user
+
+    if @item.save
+      redirect_to @item, notice: "Item created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: "Item updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy!
+    redirect_to items_path, status: :see_other, notice: "Item destroyed."
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.expect(item: [ :title, :price, :condition, :status, :category_id, :community_id ])
+    end
+end

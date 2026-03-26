dd47b27c566439aa323232ff9a5def5b54cae8e0
massivemoron345 <ancienthuman001@gmail.com>
2026-03-26 11:43:08 +0800

    basic integration of chartjs

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -3,8 +3,32 @@ import Chart from "chart.js/auto"
 
 // Connects to data-controller="chart"
 export default class extends Controller {
+  static values = {
+    type:   String,
+    labels: String, // x axis labels
+    data:   String,
+    label:  String  // legend
+  }
+
   connect() {
-    // this.chart = new Chart()
+    this.chart = new Chart(this.element, {
+      type: this.typeValue || "bar",
+      data: {
+        labels: this.labelsValue,
+        datasets: [{
+          label:              this.labelValue || "Data",
+          data:               this.dataValue,
+          borderwidth:        1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
   }
   disconnect() {
     this.chart?.destroy()

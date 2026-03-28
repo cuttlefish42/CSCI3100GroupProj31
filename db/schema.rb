af51c4ebce42fb07939f712059f38f7365bfa1a8
massivemoron345 <ancienthuman001@gmail.com>
2026-03-28 16:49:20 +0800

    added karma column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_22_184333) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_084330) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_184333) do
     t.datetime "created_at", null: false
     t.integer "default_community_id"
     t.string "email_address", null: false
+    t.integer "karma", default: 0
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.string "username"

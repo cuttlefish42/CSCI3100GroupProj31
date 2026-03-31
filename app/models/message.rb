cc6102e1c2738a9886080b8754cff91b0531540c
cuttlefish <cuttlefish256@gmail.com>
2026-03-31 15:44:12 +0800

    Added turbo stream

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,7 @@ class Message < ApplicationRecord
 
   belongs_to :item, optional: true
   belongs_to :offer, optional: true
+
+  # update <div id="messages">
+  after_create_commit -> { broadcast_append_to conversation, target: "messages" }
 end

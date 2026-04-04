beb01dfe226d6d4bbae0155a25795ee499e86be5
massivemoron345 <ancienthuman001@gmail.com>
2026-04-04 20:57:10 +0800

    first implementation of pending transaction

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,12 +5,20 @@ class DashboardsController < ApplicationController
     @recv_sort = params[:recv_sort] || "date"
     @recv_dir = params[:recv_dir] == "asc" ? :asc : :desc
 
-    @sent_offers = Current.user.offers
-                       .includes(item: :seller)
-                       .sorted_by(@sent_sort, @sent_dir, scope: :sent)
+    @sent_offers = 
+      Current.user.offers
+        .includes(item: :seller)
+        .sorted_by(@sent_sort, @sent_dir, scope: :sent)
 
-    @received_offers = Offer.received_by(Current.user)
-                            .includes(:buyer, :item)
-                            .sorted_by(@recv_sort, @recv_dir, scope: :received)
+    @received_offers = 
+      Offer.received_by(Current.user)
+        .includes(:buyer, :item)
+        .where.not(status: "Accepted")
+        .sorted_by(@recv_sort, @recv_dir, scope: :received)
+    
+    @pending_transactions = 
+      Offer.includes(:item)
+        .joins(:item)
+        .where(items: { seller_id: Current.user.id }, offers: { status: "Accepted" })
   end
 end

16ba8e648b644f34d763538bb92383e70cbbe170
cuttlefish <cuttlefish256@gmail.com>
2026-04-05 14:53:48 +0800

    lint

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,19 +5,19 @@ class DashboardsController < ApplicationController
     @recv_sort = params[:recv_sort] || "date"
     @recv_dir = params[:recv_dir] == "asc" ? :asc : :desc
 
-    @sent_offers = 
+    @sent_offers =
       Current.user.offers
         .includes(item: :seller)
         .where.not(status: "accepted")
         .sorted_by(@sent_sort, @sent_dir, scope: :sent)
 
-    @received_offers = 
+    @received_offers =
       Offer.received_by(Current.user)
         .includes(:buyer, :item)
         .where.not(status: "accepted")
         .sorted_by(@recv_sort, @recv_dir, scope: :received)
-    
-    @pending_transactions = 
+
+    @pending_transactions =
       Offer.includes(:item, :buyer)
         .joins(:item)
         .where(status: "accepted")

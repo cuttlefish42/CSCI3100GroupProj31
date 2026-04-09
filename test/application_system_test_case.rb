9ff6fbe0729eb7569f1d516ad45d8f1bc047ffb7
massivemoron345 <ancienthuman001@gmail.com>
2026-04-09 16:48:29 +0800

    init

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+require "capybara/minitest"
+require "selenium/webdriver"
+require_relative "system/support/bdd_steps"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include BddSteps
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+end

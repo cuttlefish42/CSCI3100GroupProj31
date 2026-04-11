fa238ea60fc478f44ae51d3dea6996f16ef9d8a5
cuttlefish <cuttlefish256@gmail.com>
2026-04-12 01:25:36 +0800

    Add daily digest email feature

---
+++ app/mailers/digest_mailer.rb
@@ -0,0 +1,10 @@
+class DigestMailer < ApplicationMailer
+    def daily_digest(user)
+    @user = user
+    @new_listings = ::Listing.where("created_at > ?", 24.hours.ago)
+                           .order(created_at: :desc)
+                           .limit(20)
+    
+    mail(to: @user.email_address, subject: "Your Daily Marketplace Digest")
+  end
+end

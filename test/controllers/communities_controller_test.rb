cc75b631d47be736c3859cd528824e442fb42480
cuttlefish <cuttlefish256@gmail.com>
2026-04-13 03:33:06 +0800

    add join/leave and my feed tests

--- test/controllers/communities_controller_test.rb
+++ test/controllers/communities_controller_test.rb
@@ -40,4 +40,34 @@ class CommunitiesControllerTest < ActionDispatch::IntegrationTest
     }
     assert_redirected_to new_session_path
   end
+
+  test "user can join a community" do
+    sign_in_as @non_admin
+    assert_difference "CommunityMembership.count", 1 do
+      post join_community_path(@community)
+    end
+    assert_redirected_to community_path(@community)
+  end
+
+  test "user can leave a community" do
+    sign_in_as users(:two)
+    assert_difference "CommunityMembership.count", -1 do
+      delete leave_community_path(@community)
+    end
+    assert_redirected_to community_path(@community)
+  end
+
+  test "admin cannot leave community" do
+    sign_in_as @admin
+    assert_no_difference "CommunityMembership.count" do
+      delete leave_community_path(@community)
+    end
+    assert_equal "Admins cannot leave their community.", flash[:alert]
+  end
+
+  test "my feed filters items to joined communities" do
+    sign_in_as users(:two)
+    get items_path(feed: "my")
+    assert_response :success
+  end
 end

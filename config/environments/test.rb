2cdc3feaa7549234019bb5a88b57d2ff943bf9a2
LAM KEI HUNG <91014750+martinlam-owner@users.noreply.github.com>
2026-04-12 18:34:50 +0000

    mail sys test 3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
+  # Execute background jobs inline in tests (for deliver_later, etc.)
+  config.active_job.queue_adapter = :inline
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 

83c9fe07c537dee1435cc92d300d6835a720fef3
cuttlefish <cuttlefish256@gmail.com>
2026-04-14 16:23:45 +0800

    use SMTP_USERNAME as mailer from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@group31.com"
+  default from: ENV.fetch("SMTP_USERNAME", "noreply@group31.com")
   layout "mailer"
   helper :application
 end
